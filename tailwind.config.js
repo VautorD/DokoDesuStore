5eb4aa9298281939ac338ee4c9985bbdb41706cd
Leteinturier Alix <al.leteinturier@gmail.com>
2024-06-25 18:26:28 +0200

    Debut front page tout les produit et produit détails

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',
+    './assets/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#1DACB9',
+        'secondary': '#F28EAA',
+        'nav-bg': '#FBFBFB',
+        'bg': '#EDEFF2',
+        'primary-Text': '#111928',
+        'secondary-Text': '#8899A8',
+        'strock': '#DFE4EA'
+      },
+      boxShadow: {
+        'custom': '0px 4px 4px rgba(0, 0, 0, 0.1)',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      padding: {
+        '15': '90px',
+        '30': '120px',
+      },
+    },
+  },
+  plugins: [],
+}

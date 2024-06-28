2e950449a456bf85ded1a4307f81e3de5d50a240
V.D <dorianevtr@gmail.com>
2024-06-28 15:16:41 +0200

    Update hompage et boutique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\BoutiqueRepository;
+use App\Repository\CategorieBRepository;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(CategorieBRepository $categorieBRepository, ProduitRepository $produitRepository, BoutiqueRepository $boutiqueRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categoriesB' => $categorieBRepository->findBy([], ['id' => 'asc']),
+            'boutiques' => $boutiqueRepository->findBy([], ['id' => 'asc']),
+            'produits' => $produitRepository->findBy([], ['id' => 'asc'])
         ]);
     }
 }
